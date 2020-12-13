2833e89623f42e9b13386ce68ab2b4ddb972c8f6
Fedor Shamakhov <fedor.shamakhov@gmail.com>
2020-12-13 16:07:04 +0800

    🎁 Prettier fix imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
 import { CatsService } from './cats/cats.service';
